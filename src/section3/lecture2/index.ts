28a40d7f2d7c263fddf7213779ddb19a14ff0156
yuncoffee <dong072815@gmail.com>
2022-11-10 22:12:41 +0900

    [study] 2-1, 2-2 study #4

---
+++ src/section3/lecture2/index.ts
@@ -0,0 +1,15 @@
+// 모듈 만들기
+
+// node module
+// const utils = require("./utils") // namespace exports
+// const { odd, even } = require("./utils") // 구조분해 할당
+
+// js module
+// namespace exports
+// import utils from "./utils"
+
+// 구조분해 할당
+import { odd, even } from "./utils"
+
+console.log(odd)
+console.log(even)
---
+++ src/section3/lecture2/utils.ts
@@ -0,0 +1,14 @@
+// 모듈 만들기
+
+// const odd = "홀수"
+// const even = "짝수"
+
+// node module
+// module.exports = { odd, even }
+
+// js module
+// export default { odd, even } //default module
+
+// 개별 export
+export const odd = "홀수"
+export const even = "짝수"
