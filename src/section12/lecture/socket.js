28026bff6ea6b2157c414401c9f00c206d070799
yuncoffee <dong072815@gmail.com>
2023-01-02 13:46:15 +0900

    study : 11-2 #21

--- src/section12/lecture/socket.js
+++ src/section12/lecture/socket.js
@@ -1,33 +1,64 @@
-const WebSocket = require("ws")
+// const WebSocket = require("ws")
+
+// module.exports = (server) => {
+//     const wss = new WebSocket.Server({ server })
+
+//     wss.on("connection", (ws, req) => {
+//         // 웹소켓 연결 시
+//         const ip =
+//             req.headers["x-forwarded-for"] || req.connection.remoteAddress
+//         console.log("새로운 클라이언트 접속", ip)
+//         ws.on("message", (message) => {
+//             // 클라이언트로부터 메시지
+//             console.log(message.toString())
+//         })
+//         ws.on("error", (error) => {
+//             // 에러 시
+//             console.error(error)
+//         })
+//         ws.on("close", () => {
+//             // 연결 종료 시
+//             console.log("클라이언트 접속 해제", ip)
+//             clearInterval(ws.interval)
+//         })
+
+//         ws.interval = setInterval(() => {
+//             // 3초마다 클라이언트로 메시지 전송
+//             if (ws.readyState === ws.OPEN) {
+//                 ws.send("서버에서 클라이언트로 메시지를 보냅니다.")
+//                 ws.send("hello! it's me!")
+//             }
+//         }, 3000)
+//     })
+// }
+
+const SocketIO = require("socket.io")
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server })
+    const io = SocketIO(server, { path: "/socket.io" })
 
-    wss.on("connection", (ws, req) => {
+    io.on("connection", (socket) => {
         // 웹소켓 연결 시
+        const req = socket.request
         const ip =
             req.headers["x-forwarded-for"] || req.connection.remoteAddress
-        console.log("새로운 클라이언트 접속", ip)
-        ws.on("message", (message) => {
-            // 클라이언트로부터 메시지
-            console.log(message.toString())
+        console.log("새로운 클라이언트 접속!", ip, socket.id, req.ip)
+        socket.on("disconnect", () => {
+            // 연결 종료 시
+            console.log("클라이언트 접속 해제", ip, socket.id)
+            clearInterval(socket.interval)
         })
-        ws.on("error", (error) => {
+        socket.on("error", (error) => {
             // 에러 시
             console.error(error)
         })
-        ws.on("close", () => {
-            // 연결 종료 시
-            console.log("클라이언트 접속 해제", ip)
-            clearInterval(ws.interval)
+        socket.on("reply", (data) => {
+            // 클라이언트로부터 메시지
+            console.log(data)
         })
-
-        ws.interval = setInterval(() => {
+        socket.interval = setInterval(() => {
             // 3초마다 클라이언트로 메시지 전송
-            if (ws.readyState === ws.OPEN) {
-                ws.send("서버에서 클라이언트로 메시지를 보냅니다.")
-                ws.send("hello! it's me!")
-            }
+            socket.emit("news", "Hello Socket.IO")
         }, 3000)
     })
 }
